ab78a44e3259c03398f1a8da9a585f62fd1c997a
Satinder Singh <satinder@rainsphere.ai>
2023-07-27 07:07:47 -0700

    attempt to fix ignore file patterns, no luck

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -68,7 +68,7 @@ return {
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
-        file_ignore_patterns = { "vendor/.*" },
+        file_ignore_patterns = { "/vendor/", "vendor/", "^vendor/" },
       },
     },
   },
